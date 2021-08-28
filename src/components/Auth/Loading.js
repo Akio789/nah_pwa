a526e00b446fcb617601a023a98e2395f5f713ec
Akio Olvera <akiooarao@gmail.com>
2021-08-28 14:10:45 -0600

    Custom loader

--- src/components/Auth/Loading.js
+++ src/components/Auth/Loading.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
+import styles from './Loading.module.css';
 
 const Loading = () => {
   return (
-    <div>
-      Loading...
+    <div className={styles.loader}>
+      <Spinner animation="border" variant="primary" className='mb-2' />
+      <h3>Loading...</h3>
     </div>
   )
 };
