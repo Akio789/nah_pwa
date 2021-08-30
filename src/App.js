9b7cb0a1ec64e42c34b5cb8549c4e2906333f95a
Akio Olvera <akiooarao@gmail.com>
2021-08-30 16:11:00 -0600

    Removing amplify auth

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import {
   Route
 } from "react-router-dom";
 
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-
 import Home from './displays/Home/Home';
 import Login from './displays/Login/Login';
 import Header from './components/Header/Header';
 import ProtectedRoute from "./components/Auth/ProtectedRoute";
-
-Amplify.configure(awsconfig);
 
 const App = () => {
   return (
@@ -29,4 +24,4 @@ const App = () => {
   )
 }
 
-export default withAuthenticator(App);
+export default App;
