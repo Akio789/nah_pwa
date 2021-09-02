ca2d7ad18d0682ec0f83da68db6c0955ee061351
Gerardo Arturo Miranda <gerardoarturomiranda@MacBook-Pro-de-Gerardo.local>
2021-09-02 15:26:54 -0500

    Initialicin Lecciones

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home from './displays/Home/Home';
 import Login from './displays/Login/Login';
 import Header from './components/Header/Header';
 import ProtectedRoute from "./components/Auth/ProtectedRoute";
-
+import Lecciones from './displays/Lecciones/Lecciones';
 const App = () => {
   const renderHeader = () => {
     if (window.location.pathname === '/') {
@@ -24,6 +24,9 @@ const App = () => {
       <Switch>
         <Route exact path="/">
           <Login />
+        </Route>
+        <Route exact path="/lecciones">
+          <Lecciones />
         </Route>
         <ProtectedRoute path='/home' component={Home} />
       </Switch>
