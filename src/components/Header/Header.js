86c74f941de6d5c3768fb3e240556907f3cc25d6
Akio Olvera <akiooarao@gmail.com>
2021-09-19 18:50:23 -0600

    Putting logout btn in navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,21 +3,25 @@ import { NavLink } from 'react-router-dom';
 import logoNAH from '../../assets/export.png';
 import styles from './Header.module.css';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import LogoutButton from '../../components/Auth/LogoutButton'
 
 const Header = ({ latestLesson }) => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">N A H</Navbar.Brand>
+                <Navbar.Brand href="/home">N A H</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="/home">Home</Nav.Link>
                         <Nav.Link href={'/lecciones/' + latestLesson}>Lecciones</Nav.Link>
                         <Nav.Link href="#link">Códices</Nav.Link>
                         <Nav.Link href="#link">Mapas</Nav.Link>
                         <Nav.Link href="#link">ML</Nav.Link>
                         <Nav.Link href="#link">Glorario</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <LogoutButton />
                     </Nav>
                 </Navbar.Collapse>
             </Container>
